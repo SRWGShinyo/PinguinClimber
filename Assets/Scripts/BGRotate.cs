1daf1b34c0bfea5c4186a1f470971126f53e2404
Yohann DE <yohann.degli-esposti@epita.fr>
2020-07-27 16:47:21 +0200

    fixes: added a better UI, fixed a rigidbody bug and rotating the BG now.

---
+++ Assets/Scripts/BGRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGRotate : MonoBehaviour
+{
+    float rotate = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate += Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 + rotate));
+    }
+}
